02f90228219bf3c65853b9e2b8a6f3400ae827c7
massoncorlette <massoncorlette07@gmail.com>
2024-05-12 01:47:03 -0400

    having a hard time thinking conceptually on how to write the logic first before the DOM

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const boardSelector = document.querySelector('#board-container');
-
 function Gameboard() {
   const rows = 3;
   const columns = 3;
   const board = [];
-
 
   for (let i = 0; i < rows; i++) {
     board[i] = [];
@@ -14,15 +11,63 @@ function Gameboard() {
   }
   console.log(board);
   return board;
+
+  //trying to get used to es6
+  const getBoard = () => {
+    return board;
+  };
+  //this is the traditional way, not es6
+  //getting the cordinate value of 2D array?
+  const getCellValue = function(cell) {
+    return cell
+  };
+
+
+
+  
+}
+
+function gameController(playerOne,Playertwo) {
+
+  playerOneName = playerOneName || "Player One";
+  playerTwoName = playerTwoName || "Player Two";
+
+  const board = Gameboard();
+
+  // make player object with two arrays
+  const playersObject = [{name: playerOneName, token: 1}, 
+  {name: playerTwoName, token: 2}];
+
+  const activePlayer = players[0];
+
+  const switchPlayerTurn = function() {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = function() {
+    return activePlayer;
+  };
+
+  const printNewRound = function() {
+    board.printBoard();
+    console.log(getActivePlayer().name + "'s turn.");
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  // printing updated board alongside who's turn it is
+  printNewRound();
+
+};
+
+
+const createGamer = function(playerSelect, symbol) {
+  return (playerSelect, symbol);
 }
 
 Gameboard();
 
-function displayController() {
 
-}
 
-function createGamer(playerSelect, symbol) {
-  return (playerSelect, symbol);
-}
 
