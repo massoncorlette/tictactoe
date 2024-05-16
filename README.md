1e6df68c2c5aa580adce95158cfd643c216bd147
massoncorlette <massoncorlette07@gmail.com>
2024-05-16 05:42:50 -0400

    not making good progress, feel overwhelmed how to go about this, I have revisited all the notes and lessons and asked for help, but still can't seem to think clearly right now

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Some beginning pseudo:
 /////////////////
 gameBoard()
 
+  function to populate 1-3s on cells
+
 	function to return updated board
 
 	function to array.filter through board array (player drop symbol)
@@ -17,6 +19,7 @@ gameFlow()
 	
 	function for prompt (player chooses X or O)
 		assign symbol to Gamer(player,symbol)
+    call for gameBoard().populate cells
 	
 	function to switch player turn
 
--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-
 const gameBoard = function () {
-  const boardArray = ['', '', '', '', '', '', '', '', ''];
+  const boardArray = [];
+  const cells = 9; 
+
+  for (let i = 0; i < cells; i++) {
+    boardArray[i] = i;
+  }
 
   const updatedBoard = function() {
     return boardArray;
   };
 
+  const dropToken = function(symbol,cell) {
+    const pickedCell = cell;
+
+    return pickedCell = symbol;
+  }
+
 };
 
-
-
-const Gamer = function(player,symbol) {
+const Gamer = function(player,symbol,points) {
   this.player = player;
   this.symbol = symbol;
+  this.points = points;
 };
 
-const gameFlow = function () {
-  const board = gameBoard;
+const gameFlow = function (playerOne, playerTwo) {
+  const board = gameBoard();
+  let activePlayer = playerOne;
+
+  function switchPlayerTurn() {
+    if (activePlayer === playerOne) {
+        activePlayer = playerTwo;
+    } else {
+        activePlayer = playerOne;
+    }
+  }
+
+
 
 };
 
+const boardContainer = document.querySelector('#board-container');
+
+for (let i = 0; i < 9; i++) {
+  const newCell = document.createElement('div');
+  newCell.classList.add('board-divs');
+  boardContainer.appendChild(newCell);
+}
 
 
 
